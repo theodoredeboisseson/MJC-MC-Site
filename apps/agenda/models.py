a4a277c6db6e08779f2179f79936f8825501a9aa
Théodore de Boisseson <theodoredeboisseson@gmail.com>
2025-02-25 15:15:09 +0100

    Ajout des "Événements passés" et "Événements à venir" avec tri et pagination

--- apps/agenda/models.py
+++ apps/agenda/models.py
@@ -2,6 +2,7 @@ from wagtail.models import Page
 from django.db import models
 from wagtail.admin.panels import FieldPanel
 from django.core.paginator import Paginator
+from datetime import datetime
 
 from apps.common.models import DetailPage
 
@@ -9,37 +10,48 @@ from apps.common.models import DetailPage
 class AgendaIndexPage(Page):
     intro = models.TextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro'),
-    ]
+    content_panels = Page.content_panels + [FieldPanel('intro')]
 
-    def get_events(self):
-        # Get list of events ordered by date
-        return EventPage.objects.live().order_by('date')
+    def get_upcoming_events(self, sort_by='date'):
+        return EventPage.objects.live().filter(date__gte=datetime.now()).order_by(sort_by, 'title')
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-
-        # Get all events
-        all_events = self.get_events()
-
-        # Paginate events list
-        paginator = Paginator(all_events, 9)  # Show 9 events per page
-        page = request.GET.get('page')
-        events = paginator.get_page(page)
-
-        context['events'] = events
+        sort_by = request.GET.get('sort_by', 'date')
+        paginator = Paginator(self.get_upcoming_events(sort_by), 9)
+        context['upcoming_events'] = paginator.get_page(request.GET.get('page'))
+        context['past_events_page'] = self.get_children().type(PastEventsPage).live().first()
+        context['sort_by'] = sort_by
         return context
 
     class Meta:
         verbose_name = "Page Agenda"
 
+
+class PastEventsPage(Page):
+    intro = models.TextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel('intro')]
+
+    def get_past_events(self, sort_by='-date'):
+        return EventPage.objects.live().filter(date__lt=datetime.now()).order_by(sort_by, 'title')
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        sort_by = request.GET.get('sort_by', '-date')
+        paginator = Paginator(self.get_past_events(sort_by), 9)
+        context['past_events'] = paginator.get_page(request.GET.get('page'))
+        context['sort_by'] = sort_by
+        return context
+
+    class Meta:
+        verbose_name = "Événements passés"
+
+
 class EventPage(DetailPage):
     date = models.DateTimeField("Date de l'événement")
 
-    content_panels = DetailPage.content_panels + [
-        FieldPanel('date'),
-    ]
+    content_panels = DetailPage.content_panels + [FieldPanel('date')]
 
     class Meta:
         verbose_name = "Événement"
