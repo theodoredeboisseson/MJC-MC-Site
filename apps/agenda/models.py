a53bce651be5c6c767a33ecc301c0d217971ab62
Théodore de Boisseson <theodoredeboisseson@gmail.com>
2025-02-25 17:19:57 +0100

    Refactor encapsulation templates évènements ~ agenda/model

--- apps/agenda/models.py
+++ apps/agenda/models.py
@@ -6,16 +6,14 @@ from datetime import datetime
 
 from apps.common.models import DetailPage
 
-
-def get_filtered_events(date_filter, sort_by, villes=None):
+def get_events(date_filter, sort_by, villes=None):
     events = EventPage.objects.live().filter(date_filter)
     if villes:
         ville_list = villes.split(',')
-        events = events.filter(
-            models.Q(ville__in=ville_list) | models.Q(ville=EventPage.BOTH)
-            if len(ville_list) == 2
-            else models.Q(ville=ville_list[0]) | models.Q(ville=EventPage.BOTH)
-        )
+        if len(ville_list) == 2:  # Si les deux villes sont sélectionnées
+            events = events.filter(models.Q(ville__in=ville_list) | models.Q(ville=EventPage.BOTH))
+        else:
+            events = events.filter(models.Q(ville=ville_list[0]) | models.Q(ville=EventPage.BOTH))
     return events.order_by(sort_by, 'title')
 
 
@@ -24,23 +22,24 @@ class EventListPage(Page):
     content_panels = Page.content_panels + [FieldPanel('intro')]
 
     def get_events_context(self, request, date_filter, default_sort):
+        context = {}
         sort_by = request.GET.get('sort_by', default_sort)
         villes = request.GET.get('ville', '')
-        events = get_filtered_events(date_filter, sort_by, villes)
-        return {
-            'sort_by': sort_by,
-            'villes': villes.split(',') if villes else []
-        }, Paginator(events, 9)
+        paginator = Paginator(get_events(date_filter, sort_by, villes), 9)
+        context['sort_by'] = sort_by
+        context['villes'] = villes.split(',') if villes else []
+        return context, paginator
 
     class Meta:
         abstract = True
-
 
 class AgendaIndexPage(EventListPage):
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
         events_context, paginator = self.get_events_context(
-            request, models.Q(date__gte=datetime.now()), 'date'
+            request, 
+            models.Q(date__gte=datetime.now()), 
+            'date'
         )
         context.update(events_context)
         context['upcoming_events'] = paginator.get_page(request.GET.get('page'))
@@ -50,12 +49,13 @@ class AgendaIndexPage(EventListPage):
     class Meta:
         verbose_name = "Page Agenda"
 
-
 class PastEventsPage(EventListPage):
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
         events_context, paginator = self.get_events_context(
-            request, models.Q(date__lt=datetime.now()), '-date'
+            request, 
+            models.Q(date__lt=datetime.now()), 
+            '-date'
         )
         context.update(events_context)
         context['past_events'] = paginator.get_page(request.GET.get('page'))
@@ -66,15 +66,26 @@ class PastEventsPage(EventListPage):
 
 
 class EventPage(DetailPage):
+    MAUGUIO = 'Mauguio'
+    CARNON = 'Carnon'
+    BOTH = 'Les deux'
     VILLE_CHOICES = [
-        ('Mauguio', 'Mauguio'),
-        ('Carnon', 'Carnon'),
-        ('Mauguio et Carnon', 'Mauguio et Carnon'),
+        (MAUGUIO, 'Mauguio'),
+        (CARNON, 'Carnon'),
+        (BOTH, 'Mauguio et Carnon'),
     ]
 
     date = models.DateTimeField("Date de l'événement")
-    ville = models.CharField(max_length=10, choices=VILLE_CHOICES, default='Mauguio')
-    content_panels = DetailPage.content_panels + [FieldPanel('date'), FieldPanel('ville')]
+    ville = models.CharField(
+        max_length=10,
+        choices=VILLE_CHOICES,
+        default=MAUGUIO,
+    )
+
+    content_panels = DetailPage.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('ville'),
+    ]
 
     class Meta:
         verbose_name = "Événement"
