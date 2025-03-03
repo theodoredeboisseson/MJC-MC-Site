01be6927d045dab6df53b1e108fa7c34e9526d5b
Théodore de Boisseson <theodoredeboisseson@gmail.com>
2025-03-03 15:42:06 +0100

    Amélioration modèle et template Activité

--- apps/activites/models.py
+++ apps/activites/models.py
@@ -1,30 +1,37 @@
 from django.db import models
+from django.forms.widgets import CheckboxSelectMultiple
+from modelcluster.fields import ParentalManyToManyField, ParentalKey
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.fields import RichTextField
-from wagtail.models import Page
-from modelcluster.fields import ParentalKey
+from wagtail.models import Page, Orderable
 from modelcluster.models import ClusterableModel
+from wagtail.snippets.models import register_snippet
 
 from apps.common.models import DetailPage
 
+@register_snippet
+class Animateur(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class SubActivity(Orderable):
+    page = ParentalKey('activites.ActivityPage', related_name='sub_activities', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = RichTextField(blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('description'),
+    ]
+
+    def __str__(self):
+        return self.title
 
 class ActivityPage(DetailPage):
     DetailPage.content.verbose_name = "Description"
-    instructors = models.CharField(
-        max_length=255,
-        help_text="Nom du/des animateur(s)",
-        blank=True,
-    )
-    price_resident = models.CharField(
-        max_length=50,
-        help_text="Prix pour les résidents Mauguio/Carnon",
-        null=True,
-    )
-    price_non_resident = models.CharField(
-        max_length=50,
-        help_text="Prix pour les non-résidents",
-        null=True,
-    )
+    animateurs = ParentalManyToManyField('activites.Animateur', blank=True)
     link = models.URLField(
         max_length=200,
         help_text="Lien de redirection pour le bouton",
@@ -32,11 +39,8 @@ class ActivityPage(DetailPage):
     )
 
     content_panels = DetailPage.content_panels + [
-        FieldPanel('instructors'),
-        MultiFieldPanel([
-            FieldPanel('price_resident'),
-            FieldPanel('price_non_resident'),
-        ], heading="Tarifs"),
+        FieldPanel('animateurs', widget=CheckboxSelectMultiple),
+        InlinePanel('sub_activities', label="Sous Activités"),
         FieldPanel('link'),
     ]
 
