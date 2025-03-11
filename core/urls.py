3cd937b91fa3145df06ff9935cc9d733d202ccbc
Théodore de Boisseson <theodoredeboisseson@gmail.com>
2025-03-11 16:51:53 +0100

    feat: Ajout page plan du site

--- core/urls.py
+++ core/urls.py
@@ -23,12 +23,14 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from apps.search import views as search_views
+from apps.home import views as home_views
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
+    path("sitemap/", home_views.sitemap, name="sitemap"),
 ]
 
 
