8c9cc5fe21681afba08073624163a0f0fba04381
Théodore de Boisseson <theodoredeboisseson@gmail.com>
2025-03-14 19:09:57 +0100

    fix: update WAGTAILADMIN_BASE_URL for local development

--- core/settings/base.py
+++ core/settings/base.py
@@ -166,7 +166,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "https://127.0.0.1/"
+WAGTAILADMIN_BASE_URL = "https://0.0.0.0/"
 
 # Allowed file extensions for documents in the document library.
 # This can be omitted to allow all files
